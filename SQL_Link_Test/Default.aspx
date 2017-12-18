bc79a1bf01de0a14adcee0abce47b017c8715d1f
mingjhen <102316110@gms.tcu.edu.tw>
2017-12-18 16:30:00 +0800

    SQLTest

---
+++ SQL_Link_Test/Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+    <link rel="stylesheet" href="StyleSheet.css">
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="top">
+            <asp:TextBox ID="CountrySearch" runat="server"></asp:TextBox>
+            <asp:Button ID="LinkData" runat="server" Text="確認" OnClick="LinkData_Click" />
+        </div>
+        <div id="sidebar">
+            <div id="sidebar-top">
+                <asp:Label ID="Label1" runat="server"></asp:Label></div>
+
+            <asp:TextBox ID="TextBox1" runat="server" ReadOnly="True" Font-Size="20px" TextMode="MultiLine" Height="400px" Width="100px" BorderStyle="Solid"></asp:TextBox>
+        </div>
+        <div id="center">
+            <div id="center-top">
+                <asp:Label ID="Label2" runat="server"></asp:Label></div>
+            <asp:TextBox ID="TextBox2" runat="server" ReadOnly="True" Font-Size="20px" TextMode="MultiLine" Height="400px" Width="300px" BorderStyle="Solid" Visible="False"></asp:TextBox>
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ SQL_Link_Test/Default.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        TextBox1.Text = "";
+        //建立連接
+        SqlConnection myConn = new SqlConnection("Data Source=60.251.66.42;Initial Catalog=IPDB;Persist Security Info=True;User ID=dba;password=dba");
+         myConn.Open();
+        //打開連接
+
+        SqlCommand cmd = new SqlCommand("SELECT  distinct Country FROM [dbip-country] ", myConn);
+       // SqlCommand cmd2 = new SqlCommand("SELECT count(distinct Country) FROM [dbip-country]", myConn);
+        TextBox1.Text += "國家\n";
+       SqlDataReader dr = cmd.ExecuteReader();
+        //SqlDataReader dr2 = cmd2.ExecuteReader();
+        while (dr.Read()) {
+           // TextBox1.Text += dr["IP_Start"].ToString()+"\t";
+            TextBox1.Text += dr["Country"].ToString()+"\n";
+        }
+        cmd.Cancel();
+        dr.Close();
+        cmd = new SqlCommand("SELECT  count(distinct Country) FROM [dbip-country] ", myConn);
+        
+        //dr = cmd.ExecuteReader();
+        int c = (int)cmd.ExecuteScalar();
+        Label1.Text = "數目 :" + c;
+        myConn.Close();
+        //關閉連接
+
+    }
+
+
+    protected void LinkData_Click(object sender, EventArgs e)
+    {
+        string SC = CountrySearch.Text;
+        if (SC != "")
+        {
+            //建立連接
+            SqlConnection myConn = new SqlConnection("Data Source=60.251.66.42;Initial Catalog=IPDB;Persist Security Info=True;User ID=dba;password=dba");
+            myConn.Open();
+            //打開連接
+            SqlCommand cmd = new SqlCommand("SELECT	*   FROM [dbip-country]  where Country = '"+SC+"'", myConn);
+            SqlDataReader dr = cmd.ExecuteReader();
+            TextBox2.Text = "";
+            TextBox2.Visible = true;
+            Label2.Visible = true;
+            TextBox2.Text += "IP \t\t國家" ;
+
+            while (dr.Read())
+            {
+                TextBox2.Text += dr["IP_Start"].ToString() + "\t";
+                TextBox2.Text += dr["Country"].ToString() + "\n";
+            }
+            cmd.Cancel();
+            dr.Close();
+            cmd = new SqlCommand("SELECT  count(*) FROM [dbip-country] where Country = '" + SC + "'", myConn);
+
+            int c = (int)cmd.ExecuteScalar();
+            Label2.Text = "數目 :" + c;
+
+            cmd.Cancel();
+            myConn.Close();
+            //關閉連接
+        }
+        else {
+            TextBox2.Visible = false;
+            Label2.Visible = false;
+            Response.Write("<Script language='Javascript'>");
+            Response.Write("alert('未輸入搜尋國家')");
+            Response.Write("</" + "Script>");
+        }
+    }
+        
+}
